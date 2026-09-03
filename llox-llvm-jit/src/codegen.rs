320295d5e87ea86b32d67aadede265765ba1228d
Lo3wer <leozhang226@gmail.com>
2026-09-03 12:14:49 -0600

    feat (llox) created loxvalue tagged structs for llvm to use when printing

--- llox-llvm-jit/src/codegen.rs
+++ llox-llvm-jit/src/codegen.rs
@@ -1,7 +1,8 @@
 use inkwell::builder::Builder;
 use inkwell::context::Context;
 use inkwell::module::{Linkage, Module};
-use inkwell::values::{FloatValue, FunctionValue};
+use inkwell::types::StructType;
+use inkwell::values::{FloatValue, FunctionValue, IntValue, StructValue};
 use inkwell::OptimizationLevel;
 use inkwell::targets::{CodeModel, InitializationConfig, RelocMode, Target, TargetMachine};
 use crate::datastructs::exceptions::CodeGenError;
@@ -11,6 +12,11 @@ use crate::datastructs::expr::Expr;
 use crate::datastructs::stmt::Stmt;
 use crate::datastructs::literal::Literal;
 use crate::datastructs::token::TokenType;
+
+const TAG_NUMBER: u64 = 0;
+const TAG_BOOL: u64 = 1;
+const TAG_NIL: u64 = 2;
+const TAG_STRING: u64 = 3;
 
 pub struct CodeGen<'ctx> {
     pub context: &'ctx Context,
@@ -53,23 +59,22 @@ impl<'ctx> CodeGen<'ctx> {
         match statement {
             Stmt::Print { expression } => {
                 let value = self.compile_expr(expression)?;
-                self.build_print_number(value)
+                self.build_print(value)
             }
             _ => Err(CodeGenError::Unsupported { token: None, message: "unsupported statement".to_string() }),
         }
     }
 
     fn declare_lox_print(&self) -> FunctionValue<'ctx> {
-        if let Some(function) = self.module.get_function("lox_print_number") {
+        if let Some(function) = self.module.get_function("lox_print_value") {
             return function;
         }
         let void_type = self.context.void_type();
-        let f64_type = self.context.f64_type();
-        let function_type = void_type.fn_type(&[f64_type.into()], false);
-        self.module.add_function("lox_print_number", function_type, Some(Linkage::External))
+        let function_type = void_type.fn_type(&[self.lox_value_type().into()], false);
+        self.module.add_function("lox_print_value", function_type, Some(Linkage::External))
     }
 
-    fn build_print_number(&self, value: FloatValue<'ctx>) -> Result<(), CodeGenError> {
+    fn build_print(&self, value: StructValue<'ctx>) -> Result<(), CodeGenError> {
         let lox_print = self.declare_lox_print();
         self.builder
             .build_call(lox_print, &[value.into()], "")
@@ -77,13 +82,49 @@ impl<'ctx> CodeGen<'ctx> {
         Ok(())
     }
 
-    fn compile_expr(&self, expr: &Expr) -> Result<FloatValue<'ctx>, CodeGenError> {
+    fn lox_value_type(&self) -> StructType<'ctx> {
+        self.context.struct_type(
+            &[self.context.i8_type().into(), self.context.i64_type().into()],
+            false,
+        )
+    }
+
+    fn make_lox_value(&self, tag: u64, bits: IntValue<'ctx>) -> StructValue<'ctx> {
+        let ty = self.lox_value_type();
+        let tag_value = self.context.i8_type().const_int(tag, false);
+        let mut value = ty.get_undef();
+        value = self.builder.build_insert_value(value, tag_value, 0, "tag").unwrap().into_struct_value();
+        value = self.builder.build_insert_value(value, bits, 1, "bits").unwrap().into_struct_value();
+        value
+    }
+
+    fn as_f64(&self, value: StructValue<'ctx>) -> FloatValue<'ctx> {
+        let bits = self.builder.build_extract_value(value, 1, "bits").unwrap().into_int_value();
+        self.builder.build_bit_cast(bits, self.context.f64_type(), "num").unwrap().into_float_value()
+    }
+
+    fn compile_expr(&self, expr: &Expr) -> Result<StructValue<'ctx>, CodeGenError> {
         // ignore id with ..
         match expr {
-            Expr::Literal {
-                value: Literal::Number(number),
-                ..
-            } => Ok(self.context.f64_type().const_float(*number)),
+            Expr::Literal { value, .. } => match value {
+                Literal::Number(number) => {
+                    let bits = self.context.i64_type().const_int(number.to_bits(), false);
+                    Ok(self.make_lox_value(TAG_NUMBER, bits))
+                }
+                Literal::Bool(boolean) => {
+                    let bits = self.context.i64_type().const_int(*boolean as u64, false);
+                    Ok(self.make_lox_value(TAG_BOOL, bits))
+                }
+                Literal::Nil => {
+                    let bits = self.context.i64_type().const_int(0, false);
+                    Ok(self.make_lox_value(TAG_NIL, bits))
+                }
+                Literal::String(string) => {
+                    let ptr = self.builder.build_global_string_ptr(string, "str").unwrap().as_pointer_value();
+                    let bits = self.builder.build_ptr_to_int(ptr, self.context.i64_type(), "strptr").unwrap();
+                    Ok(self.make_lox_value(TAG_STRING, bits))
+                }
+            },
 
             Expr::Grouping { expression, .. } => {
                 self.compile_expr(expression)
@@ -93,10 +134,12 @@ impl<'ctx> CodeGen<'ctx> {
                 let value = self.compile_expr(right)?;
 
                 match operator.token_type() {
-                    TokenType::Minus => self
-                        .builder
-                        .build_float_neg(value, "negtmp")
-                        .map_err(|error| CodeGenError::Llvm { message: error.to_string() }),
+                    TokenType::Minus => {
+                        let num = self.as_f64(value);
+                        let neg = self.builder.build_float_neg(num, "negtmp").map_err(|error| CodeGenError::Llvm { message: error.to_string() })?;
+                        let bits = self.builder.build_bit_cast(neg, self.context.i64_type(), "numbits").unwrap().into_int_value();
+                        Ok(self.make_lox_value(TAG_NUMBER, bits))
+                    }
 
                     _ => Err(CodeGenError::Unsupported { token: None, message: "unsupported unary operator".to_string()}),
                 }
@@ -110,30 +153,19 @@ impl<'ctx> CodeGen<'ctx> {
             } => {
                 let lhs = self.compile_expr(left)?;
                 let rhs = self.compile_expr(right)?;
+                let l = self.as_f64(lhs);
+                let r = self.as_f64(rhs);
 
-                match operator.token_type() {
-                    TokenType::Plus => self
-                        .builder
-                        .build_float_add(lhs, rhs, "addtmp")
-                        .map_err(|error| CodeGenError::Llvm { message: error.to_string() }),
+                let result = match operator.token_type() {
+                    TokenType::Plus => self.builder.build_float_add(l, r, "addtmp"),
+                    TokenType::Minus => self.builder.build_float_sub(l, r, "subtmp"),
+                    TokenType::Star => self.builder.build_float_mul(l, r, "multmp"),
+                    TokenType::Slash => self.builder.build_float_div(l, r, "divtmp"),
+                    _ => return Err(CodeGenError::Unsupported { token: None, message: "unsupported binary operator".to_string() }),
+                }.map_err(|error| CodeGenError::Llvm { message: error.to_string() })?;
 
-                    TokenType::Minus => self
-                        .builder
-                        .build_float_sub(lhs, rhs, "subtmp")
-                        .map_err(|error| CodeGenError::Llvm { message: error.to_string() }),
-
-                    TokenType::Star => self
-                        .builder
-                        .build_float_mul(lhs, rhs, "multmp")
-                        .map_err(|error| CodeGenError::Llvm { message: error.to_string() }),
-
-                    TokenType::Slash => self
-                        .builder
-                        .build_float_div(lhs, rhs, "divtmp")
-                        .map_err(|error| CodeGenError::Llvm { message: error.to_string() }),
-
-                    _ => Err(CodeGenError::Unsupported { token: None, message: "unsupported binary operator".to_string() }),
-                }
+                let bits = self.builder.build_bit_cast(result, self.context.i64_type(), "numbits").unwrap().into_int_value();
+                Ok(self.make_lox_value(TAG_NUMBER, bits))
             }
 
             _ => Err(CodeGenError::Unsupported { token: None, message: "unsupported expression".to_string() }),
@@ -152,8 +184,8 @@ impl<'ctx> CodeGen<'ctx> {
 
     pub unsafe fn run(&self) -> Result<i32, CodeGenError> {
         let execution_engine = self.module.create_jit_execution_engine(OptimizationLevel::None).map_err(|error| CodeGenError::Llvm { message: error.to_string()})?;
-        if let Some(lox_print) = self.module.get_function("lox_print_number") {
-            let lox_print_ptr: extern "C" fn(f64) = crate::runtime::lox_print_number;
+        if let Some(lox_print) = self.module.get_function("lox_print_value") {
+            let lox_print_ptr: extern "C" fn(crate::runtime::LoxValue) = crate::runtime::lox_print_value;
             execution_engine.add_global_mapping(&lox_print, lox_print_ptr as usize);
         }
         let function = unsafe { execution_engine.get_function::<unsafe extern "C" fn() -> i32>("llox_main") }.map_err(|error| CodeGenError::Llvm { message: error.to_string()})?;
--- llox-llvm-jit/src/runtime.rs
+++ llox-llvm-jit/src/runtime.rs
@@ -1,11 +1,34 @@
+use std::ffi::CStr;
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct LoxValue {
+    pub tag: u8,
+    pub bits: u64,
+}
+
+pub const TAG_NUMBER: u8 = 0;
+pub const TAG_BOOL: u8 = 1;
+pub const TAG_NIL: u8 = 2;
+pub const TAG_STRING: u8 = 3;
+
 #[unsafe(no_mangle)]
-pub extern "C" fn lox_print_number(x: f64) {
-    if x.fract() == 0.0 {
-        println!("{:.0}", x);
-    } else {
-        println!("{}", x);
+pub extern "C" fn lox_print_value(v: LoxValue) {
+    match v.tag {
+        TAG_NUMBER => {
+            let n = f64::from_bits(v.bits);
+            if n.fract() == 0.0 {
+                println!("{:.0}", n);
+            } else {
+                println!("{n}");
+            }
+        }
+        TAG_BOOL => println!("{}", v.bits != 0),
+        TAG_NIL => println!("nil"),
+        TAG_STRING => println!("{}", unsafe { CStr::from_ptr(v.bits as *const i8) }.to_string_lossy()),
+        _ => {}
     }
 }
 
 #[used]
-static RUNTIME_FNS: [extern "C" fn(f64); 1] = [lox_print_number];
+static RUNTIME_FNS: [extern "C" fn(LoxValue); 1] = [lox_print_value];
